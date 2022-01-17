d264a41fd9b3c1f87ab36a90b7d5759d2cf44389
kavitha <evergreenkavi@gmail.com>
2022-01-16 20:48:16 -0600

    modify

--- Product-Rest/src/main/java/com/example/demo/controller/HomeController.java
+++ Product-Rest/src/main/java/com/example/demo/controller/HomeController.java
@@ -5,45 +5,71 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.model.Book;
 import com.example.demo.model.Products;
 import com.example.demo.repository.ProductsRepository;
 
-@Controller
+@RestController
 
 public class HomeController {
 	@Autowired
 	ProductsRepository productsrepo;
-	
+
 	@RequestMapping("/list")
 	@ResponseBody
 	public List<Products> Addtotable(Products product) {
 
 		return (List<Products>) productsrepo.findAll();
 	}
-	
 
 	@RequestMapping("/prod/{pid}")
 	@ResponseBody
 	public Optional<Products> prodbyid(@PathVariable("pid") int pid) {
 
-		return  productsrepo.findById(pid);
+		return productsrepo.findById(pid);
 	}
-	
-	
-	@RequestMapping("/add")
-	
+
+	@PostMapping("/add")
 	public Products addproduct(@RequestBody Products product) {
 
-		return  productsrepo.save(product);
+		return productsrepo.save(product);
+		// return product;
+
 	}
+
+	@PutMapping("update/{id}")
+	public Products updateBookById(@RequestBody Products Products) {
+		return productsrepo.save(Products);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteBook(@PathVariable int id) {
+		productsrepo.deleteById(id);
+	}
+
+	@DeleteMapping("/deleteall")
+	public void deleteAllproducts() {
+		productsrepo.deleteAll();
+	}
+
+	@RequestMapping("/pricelist/{price}")
 	
+	public Products findByprice(@PathVariable("price") int price) {
+		return (Products) productsrepo.findByprice(price);
+		 
+	}
+
 	
+
+
 	
 }
--- Product-Rest/src/main/java/com/example/demo/repository/ProductsRepository.java
+++ Product-Rest/src/main/java/com/example/demo/repository/ProductsRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.Products;
 
 
-public interface ProductsRepository extends CrudRepository<Products, Integer>{
+public interface ProductsRepository extends JpaRepository<Products, Integer>{
 
+	List <Products> findByprice(int price);
+	
 }
